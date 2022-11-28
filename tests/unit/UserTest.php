344752b509c281ed68475312ef4e5976d10a763a
aluben2829 <aluben2829@ieselcaminas.org>
2022-11-28 19:04:12 +0100

    Finalizado

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -9,4 +9,18 @@ class UserTest extends TestCase {
         $user = new User(17);
         $this->assertEquals(false, $user->IsAnAdult());
     }
+    public function testIsAnAdultReturnsTrueForPeopleOrderThanTreshold(){
+        $user = new User(19);
+        $this->assertTrue($user->isAnAdult());
+
+        $user = new User(55);
+        $this->assertTrue($user->isAnAdult());
+
+        $user = new User(102);
+        $this->assertTrue($user->isAnAdult());  
+    }
+    public function testIsAnAdultReturnsTrueForPeopleThanTreshold(){
+        $user = new User(18);
+        $this->assertTrue($user->isAnAdult());
+    }
 }
